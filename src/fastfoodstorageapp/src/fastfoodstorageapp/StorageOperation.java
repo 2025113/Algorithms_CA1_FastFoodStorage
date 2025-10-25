57daca03efdea42c7953fad396640165f675eb4b
2025113 <2025113@student.cct.ie>
2025-10-25 11:08:37 +0100

    Commit 10: Added Javadoc-style documentation to all methods in the StorageOperation Interface, clarifying their contract purpose

--- src/fastfoodstorageapp/src/fastfoodstorageapp/StorageOperation.java
+++ src/fastfoodstorageapp/src/fastfoodstorageapp/StorageOperation.java
@@ -5,9 +5,14 @@
 package fastfoodstorageapp; 
 
 public interface StorageOperation {
+    // This is our Enqueue, the way to add a FoodItem 
     boolean addItem(FoodItem item); 
+    // This is our Dequeue, the way to remove the oldest FoodItem 
     FoodItem removeItem(); 
-    FoodItem peekTop(); 
+    // Method to view the oldest FoodItem without removing it, thats means a Peek
+    FoodItem peekTop();
+    // This line will display all items currently in storage
     void displayAll(); 
+    // This is to check if the storage has reached maximum capacity, in our case 8 trays
     boolean isFull(); 
 }
